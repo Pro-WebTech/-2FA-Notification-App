9e0cd7c67bb6c4ca4be7b7d38f9bf94b7c516fc7
Pro Software Engineer <baydev1998@gmail.com>
2023-08-06 00:23:42 +0900

    Update Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,33 +1,33 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Dashboard = () => {
-	const navigate = useNavigate();
+function Dashboard() {
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const checkUser = () => {
-			if (!localStorage.getItem("username")) {
-				navigate("/");
-			}
-		};
-		checkUser();
-	}, [navigate]);
+  useEffect(() => {
+    function checkUser() {
+      if (!localStorage.getItem("username")) {
+        navigate("/");
+      }
+    }
+    checkUser();
+  }, [navigate]);
 
-	const handleSignOut = () => {
-		localStorage.removeItem("username");
-		navigate("/");
-	};
+  function handleSignOut() {
+    localStorage.removeItem("username");
+    navigate("/");
+  }
 
-	return (
-		<div className='dashboard'>
-			<h2 style={{ marginBottom: "30px" }}>
-				Howdy, {localStorage.getItem("username")}
-			</h2>
-			<button className='signOutBtn' onClick={handleSignOut}>
-				SIGN OUT
-			</button>
-		</div>
-	);
-};
+  return (
+    <div className="dashboard">
+      <h2 style={{ marginBottom: "30px" }}>
+        Howdy, {localStorage.getItem("username")}
+      </h2>
+      <button className="signOutBtn" onClick={handleSignOut}>
+        SIGN OUT
+      </button>
+    </div>
+  );
+}
 
 export default Dashboard;
